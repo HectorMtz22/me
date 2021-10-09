271eea551e7ce0e75f506c02337b2afc193fda00
HectorMtz22 <hec.gre81@gmail.com>
2021-10-09 00:51:21 -0500

    Config

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { motion as m } from "framer-motion";
+
+import "../theme/global.css";
+
+const variants = {
+  initial: { opacity: 0 },
+  animate: { opacity: 1 },
+};
 
 const IndexPage = () => {
   return (
     <>
-      <main>Hello World!</main>
+      <m.main {...variants}>
+        <main>Hello World!</main>
+      </m.main>
     </>
   );
 };
