ff472f422a2ad379eea128c6655f505b002c1f57
HectorMtz22 <hec.gre81@gmail.com>
2021-10-09 23:55:57 -0500

    Gatsby config

---
+++ gatsby-browser.js
@@ -0,0 +1,2 @@
+import "@fontsource/open-sans"; // Defaults to weight 400.
+import "@fontsource/montserrat"; // Defaults to weight 400.
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
-    title: "blog",
+    title: "HectorMtz22",
   },
   plugins: [
     "gatsby-plugin-postcss",
